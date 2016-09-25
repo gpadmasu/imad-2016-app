531b49779b874b23d05f5b7918a861fa2a0dbd23
gpadmasu <gpadmasu@gmail.com>
2016-09-25 20:56:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
-var button=document.getElementById("counter");
+var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
     // This code executes without making a request to the counter endpoint
     counter = counter+1;
-    var spoan=doucment.getElementById("count");
+    var spoan=doucment.getElementById('count');
     span.innerHTML=counter.toString();
     
     //make request to counter endpoint
