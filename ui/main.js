366eacc99f018323e61fd84e79d0005d2816728e
gpadmasu <gpadmasu@gmail.com>
2016-10-03 01:41:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,8 @@ button.onclick=function(){
         }
     };
     //make request to counter endpoint
-    var counter=button.value;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
     request.open('GET', 'https://gpadmasu.imad.hasura-app.io/counter', true);
     request.send(null);
     
