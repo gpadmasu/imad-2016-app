ee213da7e902403af9dfe52a3d81dde86c7bc487
gpadmasu <gpadmasu@gmail.com>
2016-10-03 00:57:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,12 +32,29 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('name');
 submit.onclick = function(){
-    // make a request to the server and send the name
     
-    //capture a list of names and render it as a list
-    var names=['name1', 'name2', 'name3', 'name4'];
-    var list='';
-    for (var i=0; i<names.length; i++) list = '<li>' +names[i] +'<li>';
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    //Create a request object
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if (request.readystate===XMLHttpRequest.DONE){
+            //take some action
+            if (request.status===200){
+                //200 means request successfully completed
+                //capture a list of names and render it as a list
+                //var names=['name1', 'name2', 'name3', 'name4
+                var names=request.responseText;
+                //now convert from string back into an object using JSON
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0; i<names.length; i++) list = '<li>' +names[i] +'<li>';
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    //make request to counter endpoint
+    request.open('GET', 'https://gpadmasu.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
 };
