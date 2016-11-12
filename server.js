75183db2933c6e4ce5835e14f0a3769188650302
gpadmasu <gpadmasu@gmail.com>
2016-11-12 16:32:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ return htmlTemplate;
 }
 
 function hash(input,salt){
-    var hashed=crypto.pbkdf2Sync(input,'salt',10000,512,'sha512');
+    var hashed=crypto.pbkdf2Sync(input,'salt',100000,512,'sha512');
     // return hashed and make it printable and readable on our screen 
     return hashed.toString('hex'); 
 }
